83d689a8bc1361fd8ac5bd399cda42d3decfa014
Maxim-Pekov <max.pekov@gmail.com>
2022-12-26 01:39:37 +0300

    Move data from template to view

--- afisha_project/where_to_go/views.py
+++ afisha_project/where_to_go/views.py
@@ -2,4 +2,33 @@ from django.shortcuts import render
 
 
 def show_phones(request):
-    return render(request, 'where_to_go/index.html')
+    places = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json"
+                }
+            }
+        ]
+    }
+    return render(request, 'where_to_go/index.html', context={'places': places})
