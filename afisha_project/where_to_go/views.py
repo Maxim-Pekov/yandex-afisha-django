77f485a436a138706d3696ce8c50b725ce568e65
Maxim-Pekov <max.pekov@gmail.com>
2022-12-26 03:05:16 +0300

    Get json by Api

--- afisha_project/where_to_go/views.py
+++ afisha_project/where_to_go/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from .models import Place
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def show_place_id(request, place_id):
-    # place_id = request.params
     place = get_object_or_404(Place, pk=place_id)
-    return HttpResponse(place.title)
+    place_json = {
+        "title": place.title,
+        "imgs": [ image.img.url for image in place.imgs.all() ],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.coordinates_lng,
+            "lat": place.coordinates_lat
+        }
+    }
+    print(place_json)
+    return JsonResponse(place_json, json_dumps_params={'ensure_ascii': False, 'indent': 2})
 
 
 def show_place(request):
