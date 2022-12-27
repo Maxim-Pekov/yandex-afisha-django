b5c798c3d5fcc20d03b8ae0db0e6d5cf1f7676a1
Maxim-Pekov <max.pekov@gmail.com>
2022-12-28 00:09:09 +0300

    Install load_dotenv

--- afisha_project/afisha_project/settings.py
+++ afisha_project/afisha_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
