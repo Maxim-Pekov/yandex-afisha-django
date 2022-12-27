9e65a26dd877cdf2cfb1c3eee32962b5244a9027
Maxim-Pekov <max.pekov@gmail.com>
2022-12-27 22:21:54 +0300

    Deployment check

--- afisha_project/afisha_project/settings.py
+++ afisha_project/afisha_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ur&8u$$scpu#_!qo31o)m18n#m!oj=a9$x$g!*l0t+3in1j8u&'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 # Application definition
