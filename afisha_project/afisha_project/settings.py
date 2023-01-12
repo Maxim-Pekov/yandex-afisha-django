5c68bc448a094790f11243b2fd6961cf8ebcec21
Maxim-Pekov <max.pekov@gmail.com>
2023-01-12 21:43:41 +0300

    Delete extra commas

--- afisha_project/afisha_project/settings.py
+++ afisha_project/afisha_project/settings.py
@@ -7,9 +7,9 @@ env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = env.str('SECRET_KEY',)
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG',)
+DEBUG = env.bool('DEBUG')
 
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
