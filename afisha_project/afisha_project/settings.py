919974cbaf0e86a717bf5ade6769e70f28932948
Maxim-Pekov <max.pekov@gmail.com>
2023-01-12 13:48:45 +0300

    Fix path to staticfilesdirs in settings

--- afisha_project/afisha_project/settings.py
+++ afisha_project/afisha_project/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dirs'),
+    os.path.join(BASE_DIR, 'assets',),
 )
 
 MEDIA_URL = '/media/'
