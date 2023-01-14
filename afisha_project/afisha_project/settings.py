0ccfe20d4c45894d1dc292e314bb1a6c711147a7
Maxim-Pekov <max.pekov@gmail.com>
2023-01-14 13:40:31 +0300

    Delete if in settings

--- afisha_project/afisha_project/settings.py
+++ afisha_project/afisha_project/settings.py
@@ -96,8 +96,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets',),
 )
